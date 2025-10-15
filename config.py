4e530c9a0f6c8bbea3196c9505dc71a433fa8b24
Louis Requejo <requejolouis@gmail.com>
2025-10-15 14:25:58 -0500

    configuracion-bd

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
-    DB_HOST = '127.0.0.1'
-    DB_USER = 'root'
-    DB_PASSWORD = ''
+    DB_HOST = 'centro-salud-do-user-27592411-0.d.db.ondigitalocean.com'
+    DB_USER = 'doadmin'
+    DB_PASSWORD = 'AVNS_AxsOqAFQtHRKihJYsNb'
     DB_NAME = 'centro_salud'
-    DB_PORT = 3306
+    DB_PORT = 25060
     SECRET_KEY = 'clave_secreta_para_jwt'
