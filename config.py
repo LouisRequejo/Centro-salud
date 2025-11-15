5f8c3f38f71c0e12e47e0bf060eb436a65433c59
Leocix <leonardoguzmans06@gmail.com>
2025-11-14 23:47:48 -0500

    Aceptar cambios en los archivos

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 class Config:
-     DB_HOST = '127.0.0.1'
-     DB_USER = 'root'
-     DB_PASSWORD = ''
-     DB_NAME = 'centro_salud'
-     DB_PORT = 3306
-     SECRET_KEY = 'clave_secreta_para_jwt'
-    # DB_HOST = '127.0.0.1'
-    # DB_USER = 'root'
-    # DB_PASSWORD = ''
-    # DB_NAME = 'centro_salud'
-    # DB_PORT = 3306  # Puerto de MySQL en Laragon
-    # SECRET_KEY = 'clave_secreta_para_jwt'
+     #DB_HOST = '127.0.0.1'
+     #DB_USER = 'root'
+     #DB_PASSWORD = ''
+     #DB_NAME = 'centro_salud'
+     #DB_PORT = 3306
+     #SECRET_KEY = 'clave_secreta_para_jwt'
+    DB_HOST = 'centro-salud-do-user-27592411-0.d.db.ondigitalocean.com'
+    DB_USER = 'doadmin'
+    DB_PASSWORD = 'AVNS_AxsOqAFQtHRKihJYsNb'
+    DB_NAME = 'centro_salud'
+    DB_PORT = 25060  # Puerto de MySQL en DigitalOcean
+    SECRET_KEY = 'clave_secreta_para_jwt'
